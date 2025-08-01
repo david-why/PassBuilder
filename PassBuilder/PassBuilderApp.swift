d0abc9490a769900e81f1fba66e94b64d6a89d92
david-why <david_why@outlook.com>
2025-08-01 14:38:26 +0800

    Initial Commit

---
+++ PassBuilder/PassBuilderApp.swift
@@ -0,0 +1,17 @@
+//
+//  PassBuilderApp.swift
+//  PassBuilder
+//
+//  Created by David Wang on 2025/8/1.
+//
+
+import SwiftUI
+
+@main
+struct PassBuilderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
